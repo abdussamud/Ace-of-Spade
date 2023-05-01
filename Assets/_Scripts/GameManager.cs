eb9903ff98638fc5e496ab1424da3e404d7c2d64
RayyanUGD <71603272+abdussamud@users.noreply.github.com>
2023-05-01 22:11:19 +0500

    Shuffle assign

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,15 @@ public class GameManager : MonoBehaviour
     public int numPlayers;
     public int cardsPerPlayer;
     public List<List<Card>> playerHands = new();
+    public Transform[] cardPositions;
+    public CardsPlace[] cardPlaces;
 
     private void Start()
     {
         AceOfSpade.Shuffle(deck);
         cardsPerPlayer = deck.Count / numPlayers;
         Invoke(nameof(DealCards), 0.5f);
+        Invoke(nameof(SortCardOnTable), 1f);
     }
 
     public void DealCards()
@@ -44,5 +48,47 @@ public class GameManager : MonoBehaviour
         {
             newGameObject[i].GetComponent<Player>().hand = playerHands[i];
         }
+    }
+
+    public void SortCardOnTable()
+    {
+        float yOffset = 0;
+        float zOffset = 0;
+        int j = 0;
+        for (int i = 0; i < playerHands[0].Count; i++)
+        {
+            playerHands[0][i].gameObject.transform.position = new(cardPositions[j].position.x,
+                cardPositions[j].position.y + yOffset, cardPositions[j].position.z + zOffset);
+            playerHands[0][i].gameObject.SetActive(true);
+            if (j < 4)
+            {
+                j++;
+            }
+            else
+            {
+                j = 0;
+                yOffset -= 0.7f;
+                zOffset -= 0.1f;
+            }
+        }
+    }
+}
+
+[Serializable]
+public class CardsPlace
+{
+    public string name;
+    public int numberOfPlayers;
+    public GameObject cardsPlaceParent;
+    public Transform[] cardsPositions;
+
+    public void ActivatePlace()
+    {
+        cardsPlaceParent.SetActive(true);
+    }
+
+    public void DeactivatePlace()
+    {
+        cardsPlaceParent.SetActive(false);
     }
 }
