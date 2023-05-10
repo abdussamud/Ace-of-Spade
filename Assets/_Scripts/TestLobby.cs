ae19570fc2ee5849c5149042aeec88b2b404c58b
RayyanUGD <71603272+abdussamud@users.noreply.github.com>
2023-05-10 21:55:41 +0500

    Lobby

--- Assets/_Scripts/TestLobby.cs
+++ Assets/_Scripts/TestLobby.cs
@@ -3,9 +3,15 @@ using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class TestLobby : MonoBehaviour
 {
+    private Lobby hostLobby;
+    private float heartbeatTimer;
+
+
     private async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -17,6 +23,26 @@ public class TestLobby : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
+    private void Update()
+    {
+        HandleLobbyHeartbeat();
+    }
+
+    private async void HandleLobbyHeartbeat()
+    {
+        if (hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer < 0f)
+            {
+                float heartbeatTimerMax = 15;
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
     private async void CreateLobby()
     {
         try
@@ -24,6 +50,8 @@ public class TestLobby : MonoBehaviour
             string lobbyName = "MyLobby";
             int maxPlayer = 4;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayer);
+
+            hostLobby = lobby;
 
             Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
         }
@@ -43,6 +71,19 @@ public class TestLobby : MonoBehaviour
     {
         try
         {
+            QueryLobbiesOptions queryLobbiesOptions = new()
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+
             QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
 
             Debug.Log("Lobbies found: " + queryResponse.Results.Count);
