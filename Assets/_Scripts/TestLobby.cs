e5733a8b1ea750074538ace138fe6908aa3ce2cd
RayyanUGD <71603272+abdussamud@users.noreply.github.com>
2023-05-11 00:31:40 +0500

    Lobby Update

--- Assets/_Scripts/TestLobby.cs
+++ Assets/_Scripts/TestLobby.cs
@@ -1,15 +1,20 @@
+using System.Collections.Generic;
+using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
-using System.Collections.Generic;
-
 
 public class TestLobby : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_InputField InputField;
+    [SerializeField]
+    private TextMeshProUGUI lobbyState;
     private Lobby hostLobby;
     private float heartbeatTimer;
+    private string joinLobbyCode;
 
 
     private async void Start()
@@ -49,11 +54,14 @@ public class TestLobby : MonoBehaviour
         {
             string lobbyName = "MyLobby";
             int maxPlayer = 4;
-            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayer);
+            CreateLobbyOptions createLobbyOptions = new() { IsPrivate = true, };
+
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayer, createLobbyOptions);
 
             hostLobby = lobby;
 
-            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers + " " + lobby.Id + " " + lobby.LobbyCode);
+            lobbyState.text = "Joined";
         }
         catch (LobbyServiceException e)
         {
@@ -101,5 +109,33 @@ public class TestLobby : MonoBehaviour
     public void OnGetLobbiesListButtonClicked()
     {
         ListLobbies();
+    }
+
+    private async void JoinLobby(string lobbyCode)
+    {
+        try
+        {
+            //QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+            //_ = await Lobbies.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+
+            _ = await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
+            Debug.Log("Joined Lobby with code " + lobbyCode);
+            lobbyState.text = "Joined";
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public void OnJoinLobbyButtonClicked()
+    {
+        JoinLobby(InputField.text.ToString());
+        Debug.Log(InputField.text.ToString());
+    }
+
+    public void AddStringCode(string code)
+    {
+        joinLobbyCode = code;
     }
 }
