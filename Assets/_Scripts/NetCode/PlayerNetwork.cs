f98fa53e8d3265615701b6fa43c9de4bde3dbea7
RayyanUGD <71603272+abdussamud@users.noreply.github.com>
2023-05-29 23:45:22 +0500

    Test

--- Assets/_Scripts/NetCode/PlayerNetwork.cs
+++ Assets/_Scripts/NetCode/PlayerNetwork.cs
@@ -6,6 +6,8 @@ public class PlayerNetwork : NetworkBehaviour
 {
     private NetworkVariable<int> m_RandomNumber = new(1, NetworkVariableReadPermission.Everyone,
         NetworkVariableWritePermission.Owner);
+    private NetworkVariable<SpriteRenderer> m_ProfileColor = new(null, NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner);
 
 
     private void Start()
@@ -22,88 +24,3 @@ public class PlayerNetwork : NetworkBehaviour
     }
 
 }
-
-/*
- using Unity.Netcode;
-using UnityEngine;
-
-public class PlayerNetwork : NetworkBehaviour
-{
-    public NetworkVariable<Vector3> Position = new();
-
-    public override void OnNetworkSpawn()
-    {
-        if (IsOwner)
-        {
-            Move();
-        }
-    }
-
-    public void Move()
-    {
-        
-    }
-
-    [ServerRpc]
-    private void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
-    {
-        Position.Value = GetRandomPositionOnPlane();
-    }
-
-    private static Vector3 GetRandomPositionOnPlane()
-    {
-        Vector3 moveDirection = new();
-
-
-        if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
-        if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
-
-        float moveSpeed = 3f;
-        return moveSpeed * Time.deltaTime * moveDirection;
-    }
-
-    private void Update()
-    {
-        if (!NetworkManager.Singleton.IsServer)
-        {
-            SubmitPositionRequestServerRpc();
-        }
-        if (IsOwner && NetworkManager.Singleton.IsServer)
-        {
-            Vector3 moveDirection = new();
-
-
-            if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
-            if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
-            if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
-            if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
-
-            float moveSpeed = 3f;
-            transform.position += moveSpeed * Time.deltaTime * moveDirection;
-        }
-    }
-}
-
-
-new
-
-        //if (IsOwner)
-        //{
-        //    if (Input.GetKeyUp(KeyCode.R))
-        //    {
-        //        m_RandomNumber.Value = Random.Range(0, 100);
-        //    }
-
-        //    Vector3 moveDirection = new();
-
-        //    if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
-        //    if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
-        //    if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
-        //    if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
-
-        //    float moveSpeed = 3f;
-        //    transform.position += moveSpeed * Time.deltaTime * moveDirection;
-        //}
-*/
